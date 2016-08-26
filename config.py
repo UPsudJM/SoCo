ecb9f4ced62c1448298b5649de6cfce3cbc17918
Odile <odile.benassy@u-psud.fr>
2016-08-26 10:15:16 +0200

    Squelette venant du 'Flask Mega Tutorial' de M Grinberg.

---
+++ config.py
@@ -0,0 +1,18 @@
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'sql', 'migrate_repo')
+
+WTF_CSRF_ENABLED = True
+SECRET_KEY = 'bibliotheque pierre pactet'
+SQLALCHEMY_TRACK_MODIFICATIONS = True
+
+# mail server settings
+MAIL_SERVER = 'localhost'
+MAIL_PORT = 25
+MAIL_USERNAME = None
+MAIL_PASSWORD = None
+
+# administrator list
+ADMINS = ['odile.benassy@u-psud.fr']
