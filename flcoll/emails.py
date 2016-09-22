a1ff9b0b1079e13ace02bc8d1037433b1f617313
Odile <odile.benassy@u-psud.fr>
2016-09-22 17:46:17 +0200

    Champ Bcc: dans le mail de confirmation de l'inscription.

--- flcoll/emails.py
+++ flcoll/emails.py
@@ -7,6 +7,7 @@ from .models import Personne, Evenement
 
 def envoyer_message(subj, src, dest, text_body, html_body):
     msg = Message(subj, sender=src, recipients=dest)
+    msg.bcc = ADMINS
     msg.body = text_body
     msg.html = html_body
     mail.send(msg)
@@ -14,6 +15,6 @@ def envoyer_message(subj, src, dest, text_body, html_body):
 def confirmer_inscription(personne, evenement):
     envoyer_message('Confirmation d\'inscription au colloque "%s"' % evenement.titre.replace("'", "\\\'"),
                    ADMINS[0],
-                   [personne.email], #ADMINS],
+                   [personne.email],
                    render_template("confirmation_inscription.txt", personne=personne, evenement=evenement),
                    render_template("confirmation_inscription.html", personne=personne, evenement=evenement))
