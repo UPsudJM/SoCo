c93a74063002ceaa1c50887fd01dbe0e7f3ce181
Odile <odile.benassy@u-psud.fr>
2016-09-26 16:18:54 +0200

    Début tests unitaires.

---
+++ tests/context.py
@@ -0,0 +1,23 @@
+import sys, os
+import unittest
+
+basedir = os.path.dirname(os.path.abspath(__file__))
+sys.path.insert(0, basedir + '/../')
+
+from flcoll import app
+
+class TestCase(unittest.TestCase):
+    def setUp(self):
+        app.config['TESTING'] = True
+        app.config['DEBUG'] = True
+        app.config['WTF_CSRF_ENABLED'] = False
+        app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'test.db')
+        self.app = app.test_client()
+        db.create_all()
+
+    def tearDown(self):
+        db.session.remove()
+        db.drop_all()
+
+if __name__ == '__main__':
+    unittest.main()
