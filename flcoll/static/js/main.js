ada04889286d7cfeafa396681094749aed1f975d
Odile <odile.benassy@u-psud.fr>
2016-10-06 12:33:23 +0200

    Test élémentaire pour fonctionnalité AngularJS.

---
+++ flcoll/static/js/main.js
@@ -0,0 +1,10 @@
+(function () {
+    'use strict';
+    angular.module('WordcountApp', [])
+        .controller('WordcountController', ['$scope', '$log',function($scope, $log) {
+            $scope.getResults = function() {
+                $log.log("test");
+            };
+        }
+                                           ]);
+}());
