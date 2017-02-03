144c8344c7817580807242863912b8b086694a7c
Odile <odile.benassy@u-psud.fr>
2017-02-03 12:38:09 +0100

    Le renommage n'était pas fini.

---
+++ soco/models.py
@@ -0,0 +1,280 @@
+import datetime
+from sqlalchemy import Table, Column, Integer, String, Text, DateTime, Date, Boolean, ForeignKey, Binary, UniqueConstraint
+from sqlalchemy.orm import relationship
+from sqlalchemy.sql import func
+from flask_restful import Resource, Api, reqparse
+from soco import Base, api, db_session
+from .texenv import escape_tex, TPL_ETIQUETTE
+
+
+personne_organisation = Table('personne_organisation', Base.metadata,
+                                  Column('id_personne', Integer, ForeignKey('personne.id')),
+                                  Column('id_organisation', Integer, ForeignKey('organisation.id'))
+                                  )
+class Personne(Base):
+    __tablename__ = 'personne'
+    __table_args__ = (UniqueConstraint('nom', 'prenom', 'email', name='uc_pers'),)
+    id = Column(Integer, primary_key = True)
+    nom = Column(String(70), nullable=False)
+    prenom = Column(String(70))
+    email = Column(String(70))
+    organisations = relationship("Organisation", secondary=personne_organisation)
+
+    def __init__(self, **kwargs):
+        Base.__init__(self)
+        for attrname in ['nom', 'prenom', 'email']:
+            if attrname in kwargs.keys():
+                setattr(self, attrname, kwargs[attrname])
+
+    def __str__(self):
+        return "%s %s" % (self.prenom, self.nom)
+
+    @classmethod
+    def chkemail(self, evt, email_a_verifier, nom_a_verifier, prenom_a_verifier):
+        p_trouvees = self.query.filter_by(email=email_a_verifier).all()
+        if not p_trouvees:
+            return [-1, "non"]
+        id_personne = 0
+        for p in p_trouvees:
+            if p.nom.lower() == nom_a_verifier.lower() and p.prenom.lower() == prenom_a_verifier.lower():
+                id_personne = p.id
+                for i in p.inscription:
+                    print(i.id_evenement)
+                    if i.id_evenement == int(evt):
+                        return [id_personne, "oui"]
+        return [id_personne, "non"]
+
+
+class Organisation(Base):
+    __tablename__ = 'organisation'
+    __table_args__ = (UniqueConstraint('nom', name='uc_orga'),)
+    id = Column(Integer, primary_key = True)
+    nom = Column(String(70), nullable=False)
+    interne = Column(Boolean, default=False)
+    email = Column(String(70))
+    logo = Column(String(200))
+    personnes = relationship("Personne", secondary=personne_organisation)
+
+    def __init__(self, **kwargs):
+        Base.__init__(self)
+        for attrname in ['nom', 'interne', 'email']:
+            if attrname in kwargs.keys():
+                setattr(self, attrname, kwargs[attrname])
+
+    def __str__(self):
+        return self.nom
+
+
+class Evenement(Base):
+    __tablename__ = 'evenement'
+    __table_args__ = (UniqueConstraint('uid_organisateur', 'date', 'titre', name='uc_even'),)
+    id = Column(Integer, primary_key = True)
+    titre = Column(String(200))
+    sstitre = Column(String(200))
+    date = Column(Date)
+    date_fin = Column(Date)
+    lieu = Column(String(400)) #, default="Faculté Jean Monnet, Salle Vedel, Université Paris Sud/Paris-Saclay")
+    resume = Column(Text)
+    gratuite = Column(Boolean, default=True)
+    uid_organisateur = Column(String(100))
+    id_entite_organisatrice = Column(Integer, ForeignKey('organisation.id'), nullable=True)
+    logo = Column(String(200))
+    upd = Column(DateTime, default=func.now(), server_default=func.now())
+    #upd = Column(DateTime)
+
+    entite_organisatrice = relationship("Organisation", back_populates="evenement")
+
+    def __init__(self, **kwargs):
+        Base.__init__(self)
+        for attrname in ['titre', 'sstitre', 'date', 'date_fin', 'lieu', 'resume', 'gratuite',
+                             'uid_organisateur', 'id_entite_organisatrice']:
+            if attrname in kwargs.keys():
+                setattr(self, attrname, kwargs[attrname])
+
+    def __repr__(self):
+        return "%s (%s)" % (self.titre, self.date)
+
+    @classmethod
+    def modif_attributs(self, evt, **kwargs):
+        e = self.query.get(evt)
+        for a in ['titre', 'sstitre', 'lieu']:
+            if kwargs.has_key(a) and kwargs[a]:
+                setattr(e, a, kwargs[a])
+        db_session.add(e)
+        try:
+            db_session.commit()
+        except:
+            raise IntegrityError("Unknown error")
+
+Organisation.evenement = relationship("Evenement", order_by=Evenement.date, back_populates="entite_organisatrice")
+
+
+class Formulaire(Base):
+    __tablename__ = 'formulaire'
+    __table_args__ = (UniqueConstraint('id_evenement', 'date_ouverture_inscriptions', name='uc_form'),)
+    id = Column(Integer, primary_key = True)
+    id_evenement = Column(Integer, ForeignKey('evenement.id'), nullable=False)
+    date_ouverture_inscriptions = Column(Date, nullable=False)
+    date_cloture_inscriptions = Column(Date, nullable=False)
+    organisateur_en_copie = Column(Boolean)
+    champ_attestation = Column(Boolean, default=True)
+    champ_type_inscription = Column(Boolean)
+    champ_restauration_1 = Column(Boolean)
+    texte_restauration_1 = Column(String(200))
+    champ_restauration_2 = Column(Boolean)
+    texte_restauration_2 = Column(String(200))
+    upd = Column(DateTime, default=func.now(), server_default=func.now())
+
+    evenement = relationship("Evenement", back_populates="formulaire")
+
+    def __init__(self, **kwargs):
+        Base.__init__(self)
+        for attrname in ['id_evenement', 'evenement', 'date_ouverture_inscriptions', 'date_cloture_inscriptions',
+                             'organisateur_en_copie', 'champ_attestation', 'champ_type_inscription',
+                             'champ_restauration_1', 'texte_restauration_1', 'champ_restauration_2', 'texte_restauration_2']:
+            if attrname in kwargs.keys():
+                setattr(self, attrname, kwargs[attrname])
+
+    def __str__(self):
+        return "%s, %s (clôt. le %s)" % (self.evenement.titre, self.evenement.date, self.date_cloture_inscriptions)
+
+    @classmethod
+    def modif_date_cloture(self, form, date):
+        f = self.query.get(form)
+        f.date_cloture_inscriptions = date
+        db_session.add(f)
+        try:
+            db_session.commit()
+        except:
+            raise IntegrityError("Unknown error")
+
+    @classmethod
+    def get_uid_organisateur(self, form):
+        f = self.query.get(form)
+        return f.evenement.uid_organisateur
+
+
+Evenement.formulaire = relationship("Formulaire", order_by=Formulaire.id, back_populates="evenement")
+
+
+class Inscription(Base):
+    __tablename__ = 'inscription'
+    __table_args__ = (UniqueConstraint('id_evenement', 'id_personne', name='uc_insc'),)
+    id = Column(Integer, primary_key = True)
+    id_evenement = Column(Integer, ForeignKey('evenement.id'), nullable=False)
+    id_personne = Column(Integer, ForeignKey('personne.id'), nullable=False)
+    date_inscription = Column(DateTime) #, default=func.now(), server_default=func.now())
+    type_inscription = Column(String(70))
+    telephone = Column(String(20))
+    organisation = Column(String(70))
+    fonction = Column(String(70))
+    badge1 = Column(String(70))
+    badge2 = Column(String(70))
+    attestation_demandee = Column(Boolean)
+    commentaire = Column(String(200))
+    inscription_repas_1 = Column(Boolean)
+    inscription_repas_2 = Column(Boolean)
+
+    evenement = relationship("Evenement", back_populates="inscription")
+    personne = relationship("Personne", back_populates="inscription")
+
+    def __init__(self, **kwargs):
+        Base.__init__(self)
+        for attrname in ['id_evenement', 'evenement', 'id_personne', 'personne', 'telephone', 'fonction', 'organisation',
+                             'date_inscription', 'badge1', 'badge2', 'type_inscription', 'attestation_demandee',
+                             'commentaire', 'inscription_repas_1', 'inscription_repas_2']:
+            if attrname in kwargs.keys():
+                setattr(self, attrname, kwargs[attrname])
+
+    def __str__(self):
+        return "%s %s" % (self.personne.prenom, self.personne.nom)
+
+    def genere_etiquette(self, base_x, base_y):
+        if len(self.badge1) < 22:
+            police1 = "\\normalsize"
+        else:
+            police1 = "\\small"
+        if len(self.badge2) < 30:
+            police2 = "\\normalsize"
+        else:
+            police2 = "\\small"
+        return TPL_ETIQUETTE % (base_x - 10, base_y + 50,
+                                    base_x, base_y, police1, escape_tex(self.badge1), police2, escape_tex(self.badge2))
+
+Evenement.inscription = relationship("Inscription", order_by=Inscription.id, back_populates="evenement")
+Personne.inscription = relationship("Inscription", order_by=Inscription.id, back_populates="personne")
+
+# API RESTful
+@api.resource('/api/chkemail/')
+class ChkEmail(Resource):
+    def get(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument('evt', required=True, help="Evenement cannot be blank!")
+        parser.add_argument('email', required=True, help="Email cannot be blank!")
+        parser.add_argument('nom', required=True, help="Nom cannot be blank!")
+        parser.add_argument('prenom', required=True, help="Prenom cannot be blank!")
+        args = parser.parse_args()
+        try:
+            evt = int(args['evt'])
+        except:
+            raise ValueError("'%s' is not a valid event id" % args['evt'])
+        return Personne.chkemail(evt, args['email'], args['nom'], args['prenom'])
+
+@api.resource('/api/envoicodeverif/')
+class CodeVerif(Resource):
+    def get(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument('email', required=True, help="Email cannot be blank!")
+        args = parser.parse_args()
+        from .emails import envoyer_code_verification
+        codeverif = envoyer_code_verification(args['email'])
+        return codeverif
+
+@api.resource('/api/modifformulaire/')
+class ModifFormulaire(Resource):
+    def get(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument('id', required=True, help="Le formulaire à modifier doit être spécifié")
+        parser.add_argument('datecloture', required=True, help="Date de clôture à modifier ?")
+        args = parser.parse_args()
+        from .emails import envoyer_mail_modification_formulaire
+        try:
+            id_formulaire = int(args['id'])
+        except:
+            raise ValueError("'%s' is not a valid form id" % args['id'])
+        try:
+            date_cloture = datetime.datetime.strptime(args['datecloture'], '%Y-%m-%d').strftime('%Y-%m-%d')
+        except:
+            raise ValueError("'%s' is not a valid date" % args['datecloture'])
+        f = Formulaire.query.get(id_formulaire)
+        if str(f.date_cloture_inscriptions) == date_cloture:
+            return
+        f.date_cloture_inscriptions = date_cloture
+        db_session.add(f)
+        try:
+            db_session.commit()
+        except:
+            raise IntegrityError("Unknown error")
+        uid_organisateur = f.evenement.uid_organisateur
+        envoyer_mail_modification_formulaire(uid_organisateur,
+                                                 f.evenement,
+                                                 date_cloture_inscriptions = f.date_cloture_inscriptions.strftime('%d/%m/%Y'))
+        return f.date_cloture_inscriptions.strftime("%d/%m/%Y")
+
+@api.resource('/api/modifevenement/')
+class ModifEvenement(Resource):
+    def get(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument('id', required=True, help="L'événement à modifier doit être spécifié")
+        parser.add_argument('titre', required=False, help="Titre à modifier ?")
+        parser.add_argument('sstitre', required=False, help="Sous-titre à modifier ?")
+        parser.add_argument('lieu', required=False, help="Lieu à modifier ?")
+        args = parser.parse_args()
+        try:
+            id_evenement = int(args['id'])
+        except:
+            raise ValueError("'%s' is not a valid form id" % args['id'])
+        from .emails import envoyer_mail_modification_formulaire
+        Evenement.modif_attributs(id_evenement, args)
+        uid_organisateur = Evenement.get_uid_organisateur(id_evenement)
+        envoyer_mail_modification_formulaire(uid_organisateur, **kwargs)
---
+++ soco/texenv.py
@@ -0,0 +1,103 @@
+from re import compile as rcompile
+from os import chdir, remove
+from subprocess import run, TimeoutExpired
+from tempfile import mkstemp
+from soco import app
+
+PDFCMD = "/usr/bin/pdflatex"
+
+LATEX_SUBS = (
+    (rcompile(r'\\'), r'\\textbackslash'),
+    (rcompile(r'([{}_#%&$])'), r'\\\1'),
+    (rcompile(r'~'), r'\~{}'),
+    (rcompile(r'\^'), r'\^{}'),
+    (rcompile(r'"'), r"''"),
+    (rcompile(r'\.\.\.+'), r'\\ldots'),
+    )
+
+TPL_ETIQUETTE="\
+\put(%d,%d){\crophrule \cropvrule}\
+\put(%d,%d){\makebox(85,50){\card{%s}{%s}{%s}{%s}}}"
+
+TPL_ETIQUETTE_VIDE="\
+\put(%d,%d){\crophrule \cropvrule}\
+\put(%d,%d){\makebox(85,50){\hspace{1cm}}}"
+
+TPL_PAGE_ETIQUETTES="\
+\\begin{picture}(210,270)(0,0)\
+\color{light-gray}\
+%s\
+\put(271,320){\cropvrule \crophrule}\
+\put(271,266){\cropvrule \crophrule}\
+\put(271,212){\cropvrule \crophrule}\
+\put(-10,156){\crophrule}\
+\put(083,156){\crophrule}\
+\put(173,156){\crophrule}\
+\
+\put(-03,149){\cropvrule}\
+\put(090,149){\cropvrule}\
+\put(180,149){\cropvrule}\
+\put(270,149){\cropvrule}\
+\
+\put(090,156){\crophrule}\
+\put(180,156){\crophrule}\
+\put(270,156){\crophrule}\
+\
+\end{picture}\
+"
+
+def escape_tex(value):
+    newval = value
+    for pattern, replacement in LATEX_SUBS:
+        newval = pattern.sub(replacement, newval)
+        return newval
+
+def genere_pdf(texcode, prefix="", timeout=10, check=True):
+    chdir("./pdf")
+    fd, texfilename = mkstemp(prefix=prefix, suffix=".tex", dir=".")
+    try:
+        t = texcode.encode("latin-1")
+    except ValueError as err:
+        print("erreur d'encodage : ", err.__doc__)
+        chdir("..")
+        return err
+    try:
+        open(texfilename, 'wb').write(t)
+    except OSError as err:
+        print("erreur d'écriture : ", err.__doc__)
+        chdir("..")
+        return err
+    try:
+        r = run([PDFCMD, texfilename], timeout=timeout)
+    except TimeoutExpired as err:
+        chdir("..")
+        print("Timeout sur processus LaTeX %s" % texfilename, err.__doc__)
+        return err
+    except err:
+        chdir("..")
+        print("Erreur sur processus LaTeX %s" % texfilename, err.__doc__)
+        return err
+    pdffilename = texfilename[:-4] + ".pdf"
+    remove(texfilename)
+    remove(texfilename[:-4] + ".log") # fichier log généré par pdflatex
+    remove(texfilename[:-4] + ".aux") # fichier aux généré par pdflatex
+    chdir("..")
+    return pdffilename
+
+def fabrique_page_etiquettes(etiquettes):
+    l = len(etiquettes)
+    try:
+        assert l <= 9
+    except AssertionError:
+        flash("Erreur dans la fabrication des étiquettes")
+        return render_template('500.html')
+    return TPL_PAGE_ETIQUETTES % "".join(etiquettes)
+
+texenv = app.create_jinja_environment()
+texenv.block_start_string = '((*'
+texenv.block_end_string = '*))'
+texenv.variable_start_string = '((('
+texenv.variable_end_string = ')))'
+texenv.comment_start_string = '((='
+texenv.comment_end_string = '=))'
+texenv.filters['escape_tex'] = escape_tex
