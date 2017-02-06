271c6e2e36e61194b9faa31c3e6a329d3babe10f
Odile <odile.benassy@u-psud.fr>
2017-02-06 13:20:38 +0100

    Intégration de Pickadate dans le code.

--- soco/static/js/soco.js
+++ soco/static/js/soco.js
@@ -99,15 +99,15 @@ var newevt = angular.module('newevt',['pickadate'])
         $scope.auj = new Date();
     };
     $scope.today();
-    $scope.calc1 = function() {
-        /* $log.log("in calc1");
-        $log.log("date = " + $scope.evenement.date); */
+    $scope.calc_date = function() {
+        $log.log("in calc_date");
+        /* $log.log("date = " + $scope.evenement.date); */
         //ncollform.date_fin.focus(); // FIXME timeout https://docs.angularjs.org/error/$rootScope/inprog?p0=$apply
         $scope.date = $scope.evenement.date;
     };
-    $scope.calc2 = function() {
-        /* $log.log("in calc2");
-        $log.log("date_fin = " + $scope.evenement.date_fin);
+    $scope.calc_date_fin = function() {
+        $log.log("in calc_date_fin");
+        /* $log.log("date_fin = " + $scope.evenement.date_fin);
         $log.log("ok ?" + ($scope.evenement.date_fin >= ncollform.date_fin.min)); */
         if (calcule_date($scope.evenement.date_fin) < calcule_date($scope.evenement.date)) {
             alert("La date de fin ne peut pas être antérieure à la date de l'événement !");
@@ -116,18 +116,18 @@ var newevt = angular.module('newevt',['pickadate'])
         $scope.date_fin = $scope.evenement.date_fin;
         ncollform.lieu.focus();
     };
-    $scope.calc3 = function() {
-        /* $log.log("in calc3");
-        $log.log("date_ouverture_inscriptions = " + $scope.evenement.date_ouverture_inscriptions); */
+    $scope.calc_date_ouverture_inscriptions = function() {
+        $log.log("in calc_ouverture_inscriptions");
+        /* $log.log("date_ouverture_inscriptions = " + $scope.evenement.date_ouverture_inscriptions); */
         if (calcule_date($scope.evenement.date_ouverture_inscriptions) > calcule_date($scope.evenement.date)) {
             alert("La date d'ouverture des inscriptions est postérieure à la date de l'évenement !");
         }
         $scope.date_ouverture_inscriptions = $scope.evenement.date_ouverture_inscriptions;
         //ncollform.date_cloture_inscriptions.focus();
     };
-    $scope.calc4 = function() {
-        /* $log.log("in calc4");
-        $log.log("date = " + $scope.evenement.date);
+    $scope.calc_date_cloture_inscriptions = function() {
+        $log.log("in calc_date_cloture_inscriptions");
+        /* $log.log("date = " + $scope.evenement.date);
         $log.log("date_fin = " + $scope.evenement.date_fin);
         $log.log("date_ouverture_inscriptions = " + $scope.evenement.date_ouverture_inscriptions);
         $log.log("date_cloture_inscriptions = " + $scope.evenement.date_cloture_inscriptions); */
