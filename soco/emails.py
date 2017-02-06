8ea7c97d44714dc51a8250a379781ce7a6b5f437
Odile <odile.benassy@u-psud.fr>
2017-02-06 13:22:16 +0100

    Oubliés dans le renommage ?
    Conflicts:
    	soco/__init__.py
    	soco/emails.py
    	soco/templates/base.html
    	soco/templates/envoi_mail_modification_formulaire.html
    	soco/templates/envoi_mail_modification_formulaire.txt
    	soco/templates/index.html

--- soco/emails.py
+++ soco/emails.py
@@ -17,7 +17,7 @@ def envoyer_message(subj, src, dest, text_body, html_body):
 
 def confirmer_inscription(email, evenement):
     from .models import Evenement
-    envoyer_message('Soco : Confirmation d\'inscription au colloque "%s"' % evenement.titre.replace("'", "\\\'"),
+    envoyer_message('SoCo : Confirmation d\'inscription au colloque "%s"' % evenement.titre.replace("'", "\\\'"),
                    ADMINS[0],
                    [email],
                    render_template("confirmation_inscription.txt", evenement=evenement),
@@ -28,7 +28,7 @@ def envoyer_code_verification(email):
     l = ['0','1','2','3','4','5','6','7','8','9']
     shuffle(l)
     codeverif = "".join(l[:4])
-    envoyer_message('Soco : Votre code de vérification',
+    envoyer_message('SoCo : Votre code de vérification',
                         ADMINS[0],
                         [email],
                         render_template("envoi_code_verification.txt", codeverif=codeverif),
@@ -46,7 +46,7 @@ def envoyer_mail_modification_formulaire(email, evenement, **kwargs):
         except:
             libelle = k
         lignes_info.append("Nouvelle valeur de %s : %s" % (libelle, v))
-    envoyer_message('Soco : Votre formulaire a été modifié',
+    envoyer_message('SoCo : Votre formulaire a été modifié',
                         ADMINS[0],
                         [email],
                         render_template("envoi_mail_modification_formulaire.txt",
