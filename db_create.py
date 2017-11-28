6153d80decb32aa237f0ce17014f83a31bc1cb47
Odile Bénassy <odile.benassy@u-psud.fr>
2017-11-28 08:37:40 +0100

    Initialisation du champ Lieu.

--- db_create.py
+++ db_create.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 from migrate.versioning import api
-#from config import SQLALCHEMY_DATABASE_URI
 from config import SQLALCHEMY_MIGRATE_REPO
 
 from soco import SQLALCHEMY_DATABASE_URI
--- soco/forms.py
+++ soco/forms.py
@@ -129,7 +129,7 @@ class NcollForm(SocoForm):
     else:
         recurrence = HiddenField(gettext('Récurrence'))
     lieu = SelectField(
-        gettext('Lieu'), coerce=int, choices = [],
+        gettext('Lieu'), coerce=int, choices = [ (0, ' -- ') ],
         description=gettext('Choisissez la salle, ou le lieu, dans la liste. S\'il ne figure pas, laissez vide'))
     date_ouverture_inscriptions = PickaDateField(gettext("Date d'ouverture des inscriptions"), objname=objname,
                                                  format='%d/%m/%Y', validators=[DataRequired()])
@@ -150,8 +150,9 @@ class NcollForm(SocoForm):
         gettext("Souhaitez-vous poser une question supplémentaire aux participant-e-s ? Si oui, cochez la case :"))
     texte_libre_1 = StringField(gettext("et précisez le texte de la question :"))
 
-    def __call__(self, **kwargs):
-        self.lieu.choices = [ (l.id, l.nom) for l in Lieu.query.order_by(Lieu.nom).all() ]
+    def __init__(self, **kwargs):
+        SocoForm.__init__(self, **kwargs)
+        self.lieu.choices += [ (l.id, l.nom) for l in Lieu.query.order_by(Lieu.nom).all() ]
 
     def validate(self):
         if not FlaskForm.validate(self):
