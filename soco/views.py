d886e339dace65689eb5c887a84f9c358321e26e
Odile <odile.benassy@u-psud.fr>
2017-12-18 18:47:41 +0100

    Essai de favicon.

--- soco/views.py
+++ soco/views.py
@@ -93,6 +93,11 @@ def internal_error(error):
     db_session.rollback()
     return render_template('500.html'), 500
 
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'),
+                                   'favicon.ico', mimetype='image/vnd.microsoft.icon')
+
 @app.route('/')
 @app.route('/index')
 # ECRIRE LES RESTRICTIONS DANS LA FONCTION
